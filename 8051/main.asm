63f8ccb4f7f18aa23a05238174d182e31ec0613f
Andrew Reilley <areill1337@gmail.com>
2018-05-17 03:52:51 -0400

    Added tons of comments

--- 8051/main.asm
+++ 8051/main.asm
@@ -5,161 +5,162 @@
     
 .org 100h
 start:
-    mov P1, 0FFh
-    mov P3, 0FFh
-    mov rand8reg, #06Eh          ; Seed random
-    mov 46h, #008h
-    lcall initser
-    lcall setupboard
-    loop:                       ; Main loop
-        lcall wait
-        lcall draw
-        lcall update
-        sjmp loop
-    gameover:
-        mov 46h, #008h
-        mov A, #0h
+    mov P1, 0h                          ; Zero the score counter
+    mov P3, 0FFh                        ; Protect the P3 inputs
+    mov rand8reg, #06Eh                 ; Seed random
+    mov 46h, #008h                      ; Initial timer value
+    lcall initser                       ; Setup the serial port
+    lcall setupboard                    ; Setup the game board
+    loop:                               ; Main loop
+        lcall wait                      ; Wait for a variable amount of time
+        lcall draw                      ; Draw the previous state
+        lcall update                    ; Update the game state
+        sjmp loop                       ; Loop endlessly
+        
+    gameover:                           ; Endless loop when the user loses
+        mov 46h, #008h                  ; Long delay
+        mov A, #0h                      ; Show the "You" image
         lcall drawimage
         lcall wait
         
-        mov 46h, #008h
-        mov A, #1h
+        mov 46h, #008h                  ; Long delay
+        mov A, #1h                      ; Show the los image
         lcall drawimage
         lcall wait
         
-        mov 46h, #003h
-        mov A, #2h
+        mov 46h, #003h                  ; Short delay
+        mov A, #2h                      ; Show the los(t) image
         lcall drawimage
         lcall wait
         
-        mov 46h, #003h
-        mov A, #3h
+        mov 46h, #003h                  ; Short delay
+        mov A, #3h                      ; Show the (l)ost image
         lcall drawimage
         lcall wait
         
-        mov 46h, #003h
-        mov A, #4h
+        mov 46h, #008h                  ; Long delay
+        mov A, #4h                      ; Show the ost image
         lcall drawimage
         lcall wait
         
         sjmp gameover
 
-wait:
-    mov R4, #0FFh
-    mov R5, #0FFh
-    mov R6, 46h
-    delay:
+wait:                                   ; Waits for an amount of time dependent on the value in 46h, and polls inputs
+    mov R4, #0FFh                       ; Reload counter
+    mov R5, #0FFh                       ; Reload counter
+    mov R6, 46h                         ; Reload outer counter to variable
+    delay:                              ; Counts down from 255*255*(value in 46h)
         djnz R4, delay
         mov R4, #0FFh
-        lcall checkinp
+        lcall checkinp                  ; Poll the buttons regularly during the loop
         djnz R5, delay
         mov R4, #0FFh
         mov R5, #0FFh
         djnz R6, delay
     ret
     
-checkinp:
-    jnb P3.2, notpressed
-    jb 40h, donecheck
+checkinp:                               ; Polls key pad and moves parts
+    jnb P3.2, notpressed                ; 3.2 is the "data available" pin on the 922
+    jb 40h, donecheck                   ; Bit 40h gets set if a button press has already been serviced
     setb 40h
     
-    jb P3.4, checkdouble
-    jb P3.3, checkleft
-    sjmp donecheck
+    jb P3.4, checkdouble                ; If 3.4 is high, either right or rotate is pressed
+    jb P3.3, checkleft                  ; If only 3.3 is high, it must be left
+    sjmp donecheck                      ; Nothing pressed
     
-    checkdouble:
+    checkdouble:                        ; Determine if right or rotate was pressed, based on 3.3
         jb P3.3, checkright
         sjmp checkrot
 
-    checkleft:
-        mov A, 41h
-        cjne A, 65h, moveleft
-        sjmp donecheck
+    checkleft:                          ; Handles left movement
+        mov A, 41h                      ; Get the X position
+        cjne A, 65h, moveleft           ; Compare it to the part's max X to determine if the part can move
+        sjmp donecheck                  ; If not, nothing to do
         moveleft:
-        inc 41h
-        lcall checkcollision
-        jb 41h, undoleft
-        lcall draw
+        inc 41h                         ; Move the part to the left
+        lcall checkcollision            ; Check for a collision in the new position
+        jb 41h, undoleft                ; Undo the movement if it has collided
+        lcall draw                      ; If not, draw the new state
         sjmp donecheck
-        undoleft:
+        undoleft:                       ; Undo the move to the left
             dec 41h
             sjmp donecheck
     
-    checkright:
-        mov A, 41h
-        cjne A, #0h, moveright
-        sjmp donecheck
+    checkright:                         ; Handles right movement
+        mov A, 41h                      ; Get the X value of the part
+        cjne A, #0h, moveright          ; If it is 0, we can't move any further to the right
+        sjmp donecheck                  ; Nothing to do if we can't move
         moveright:
-        dec 41h
-        lcall checkcollision
-        jb 41h, undoright
-        lcall draw
+        dec 41h                         ; Move the part to the right
+        lcall checkcollision            ; Check for a collision in the new position
+        jb 41h, undoright               ; If a collision occurred, undo the move
+        lcall draw                      ; Otherwise, draw the new state
         sjmp donecheck
         undoright:
-            inc 41h
+            inc 41h                     ; Undo the move to the right
             sjmp donecheck
     
-    checkrot:
-        mov R2, 40h
-        mov R0, #40h
-        mov @R0, 64h
-        lcall getpart
-        lcall checkcollision
-        jb 41h, undorot
-        lcall draw
-        sjmp donecheck
-        undorot:
-            mov 40h, R2
-            lcall getpart
+    checkrot:                           ; Handles rotation
+        mov R2, 40h                     ; Store the current part type in case we need to rotate back
+        mov R0, #40h                    ; Pointer to the current part type
+        mov @R0, 64h                    ; Set the current part type to the "next part" from our part data
+        lcall getpart                   ; Grab the new part data
+        lcall checkcollision            ; Check if the new part is in a valid position
+        jb 41h, undorot                 ; If there is a collision, undo the rotation
+        lcall draw                      ; Otherwise, draw the new state
+        sjmp donecheck                  ; Rotation complete
+        undorot:                        ; Undo the rotation if it is invalid
+            mov 40h, R2                 ; Reset the part type to the previous value
+            lcall getpart               ; Restore the part data
             sjmp donecheck
     
-    notpressed:
+    notpressed:                         ; Reset the pressed bit
         clr 40h
     donecheck:
         ret
     
-getpart:
-    mov R0, #0h
-    mov R1, #60h
-    mov DPTR, #800h
-    loadpartbyte:
-        mov A, 40h
-        mov B, #7h
+getpart:                                ; Pulls the data for a part out of prog memory and stores it at 60h
+    mov R0, #0h                         ; Counter
+    mov R1, #60h                        ; Destination, where data about the current part is stored
+    mov DPTR, #800h                     ; Origin, where all the part data is stored
+    loadpartbyte:                       ; Loop that extracts each byte
+        mov A, 40h                      ; Put the current part type into A
+        mov B, #7h                      ; Multiply by the size of a part to get the offset
         mul AB
-        add A, R0
-        movc a, @a+dptr
-        mov @R1, A
-        inc R0
-        inc R1
-        cjne R0, #7h, loadpartbyte
+        add A, R0                       ; Add the offset to counter to get the index we are copying
+        movc a, @a+dptr                 ; Get the byte from prog mem
+        mov @R1, A                      ; Save the byte into the current part data location, starting at 60h
+        inc R0                          ; Count up
+        inc R1                          ; Increment pointer to next byte
+        cjne R0, #7h, loadpartbyte      ; Loop until we hit the part size
     ret
     
-drawimage:
-    mov R0, #0h
-    mov DPTR, #700h
-    mov B, #010h
-    mul AB
-    mov DPL, A
-    loadimagebyte:
-        mov A, #0h
-        movc a, @a+dptr
-        lcall sndchr
-        inc DPL
-        inc R0
+drawimage:                              ; Send an image at position A from the program memory over serial
+    mov R0, #0h                         ; Index into the image, starts at 0
+    mov DPTR, #700h                     ; Point to the beginning of the image array
+    mov B, #010h                        ; The size of each image
+    mul AB                              ; A holds the image that we want to send, so this calculates the offset to the image
+    mov DPL, A                          ; Point DPTR to the image
+    loadimagebyte:                      ; Loop that sends each byte over serial
+        mov A, #0h                      ; A+DPTR=DPTR
+        movc a, @a+dptr                 ; Grab the byte from memory
+        lcall sndchr                    ; Send the byte
+        inc DPL                         ; Point to the next byte
+        inc R0                          ; Increment the counter
         cjne R0, #010h, loadimagebyte
     ret
     
 addpart:
-    lcall rand8
-    mov B, #12h
+    lcall rand8                         ; Get a random number for the part type
+    mov B, #12h                         ; There are 18 different parts
     div AB
-    mov 40h, B                ; Set part type to 0
+    mov 40h, B                          ; Set the random part type
     lcall getpart
-    lcall rand8
+    lcall rand8                         ; Calculate a random valid X position
     mov B, 65h
     div AB
-    mov 41h, B                ; Set part x to 4
-    mov 42h, #0h              ; Set part y to 0
+    mov 41h, B                          ; Set the part's X to a random valid position
+    mov 42h, #0h                        ; Set part y to 0
     lcall checkcollision                ; Check if the newly added part collides
     jb 41h, gotogameover                ; If it does, the game is over
     ret
@@ -208,56 +209,56 @@ update:                                 ; Called every loop to update the game s
     decfreeze:                          ; Jumped to in the case of a collision, when we need to move the part back up
         dec 42h                         ; Move the part back up
     freeze:                             ; Bakes a part into the game state array
-        mov R0, 42h
-        mov A, #30h
-        add A, R0
+        mov R0, 42h                     ; Get the Y value of the current part
+        mov A, #30h                     ; Offset that points to the start of the game state array
+        add A, R0                       ; Combine them to get a pointer to the row the part is at in the game state array
         mov R0, A
         
-        mov R2, #0h
+        mov R2, #0h                     ; Counter
         mov R1, #60h                    ; Current part start
         copypart2:
             mov A, @R1                  ; Read in current row of current part
-            mov R3, 41h
-            ls2:
+            mov R3, 41h                 ; Get the X position of the part for left shifting
+            ls2:                        ; Left shift the part into position
                 rl A
                 djnz R3, ls2
             orl A, @R0                  ; OR the row of the part with the FB row
-            mov @R0, A
-            inc R0
+            mov @R0, A                  ; Copy the row into the game state array
+            inc R0                      ; Move onto the next row by increasing all pointers and counters
             inc R1
             inc R2
-            cjne R2, #04h, copypart2
-        lcall addpart
+            cjne R2, #04h, copypart2    ; Loop over all 4 rows of the part
+        lcall addpart                   ; Since we baked in a part, we need a new one
     
-    updateend:
+    updateend:                          ; At the end of each update, check for full rows to eliminate
     
-    mov R0, #30h
-    mov R2, #10h
-    clr 42h
-    checkrow:
-        cjne @R0, #0FFh, notfull
-        inc P1
+    mov R0, #30h                        ; Pointer to game state array
+    mov R2, #10h                        ; Counter set to size of game state array
+    clr 42h                             ; No full rows yet
+    checkrow:                           ; Loop to check each row
+        cjne @R0, #0FFh, notfull        ; Check if the current row is full (every bit set)
+        inc P1                          ; If it is, increase the score
         
-        mov @R0, #0h
-        mov 3, 0
-        mov 1, 0
+        mov @R0, #0h                    ; Clear out the row
+        mov 3, 0                        ; Stash the current place in the search
+        mov 1, 0                        ; Create a pointer to the previous (above) row
         dec R1
         
-        moverow:
-            mov A, @R1
-            mov @R0, A
-            dec R0
+        moverow:                        ; Loop the brings down each row to fill in the empty row
+            mov A, @R1                  ; Pull out the row above
+            mov @R0, A                  ; Copy it into the row below
+            dec R0                      ; Move up both pointers
             dec R1
-            cjne R1, #2Fh, moverow
+            cjne R1, #2Fh, moverow      ; Loop until we have copied the top row into the second from the top
         
         mov 0, 3    
-        notfull:
-            inc R0
-            djnz R2, checkrow
+        notfull:                        ; If the row isn't full, continue the search
+            inc R0                      ; Check the next row
+            djnz R2, checkrow           ; Continue the loop until all rows have been checked
     jb 42h, speedup
     ret
-    speedup:
-        dec 46h
+    speedup:                            ; Speeds up the game if any rows have been cleared
+        dec 46h                         ; Decreases delay between updates
         ret
 
 draw:                                   ; Outputs the game state over serial
@@ -358,7 +359,7 @@ rand8b:	anl	a, #10111000b
 	mov	rand8reg, a
 	ret
 
-.org 700h
+.org 700h                               ; Image data. 16 bytes per image.
     ; You
     .db 0b01100110
     .db 0b01100110
@@ -447,16 +448,16 @@ rand8b:	anl	a, #10111000b
     .db 0b00011000
 
 
-.org 800h    
+.org 800h                               ; Part data. 7 bytes per part.
 parts:
     ; Part 0, L
-    .db 0b00000000
-    .db 0b00000010
-    .db 0b00000010
-    .db 0b00000011
-    .db 0x1
-    .db 0x6
-    .db 0x1
+    .db 0b00000000                      ; Row 0 of image
+    .db 0b00000010                      ; Row 1 of image
+    .db 0b00000010                      ; Row 2 of image
+    .db 0b00000011                      ; Row 3 of image
+    .db 0x1                             ; Index of part that results from a clockwise rotation
+    .db 0x6                             ; Maximum distance to the left the part can move (number of 0's on the left in the image)
+    .db 0x1                             ; Empty space above the part (number of 0's above it in the image)
     ; Part 1, L
     .db 0b00000000
     .db 0b00000000
